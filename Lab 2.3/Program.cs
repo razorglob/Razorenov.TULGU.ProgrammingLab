64d30ea4cfb11c7c88f230730cc6f10537cf6dba
Вячеслав Разоренов <vrazorenov@gmcs.ru>
2019-10-20 23:39:12 +0300

    add lab 2.3

---
+++ Lab 2.3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a= ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b= ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("с= ");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0}+{1}+{2}={3}", a, b, c, a + b + c);
+            Console.Read();
+
+        }
+    }
+}
